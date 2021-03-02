c6d7a314afe5d71faf2db40f16ac72978694a764
igorgato <igormqg@gmail.com>
2021-03-02 08:54:58 -0300

    permitAll de apenas usuario

--- backEnd/blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
+++ backEnd/blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -31,7 +31,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception{
 		http.authorizeRequests()
 		.antMatchers("/usuarios/logar").permitAll()
-		.antMatchers("/**").permitAll()
+		.antMatchers("/usuarios/*").permitAll()
 		.antMatchers("/usuarios/cadastrar").permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
